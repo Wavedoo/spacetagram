19a36fe4c133540771e6cd6965f3e17366ecb731
Sterling <sterling1@hotmail.ca>
2022-01-15 16:41:57 -0500

    Final Version
    Organized the files to be more appropriate. Everything should be working properly.

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-//Inital setup
+//Inital setup with default mode being random images
 const API_TOKEN = "TfTsHtqDPttmSZcKm77S99loINsheU9QC02NcpcF";
 var display = document.getElementById("displayArea");
-var link = `https://api.nasa.gov/planetary/apod?api_key=${API_TOKEN}&count=10`;
-getData(link);
+var url = `https://api.nasa.gov/planetary/apod?api_key=${API_TOKEN}&count=5`;
+getData(url);
 
-var likedDates = [];
-likedDates = localStorage.getItem("likes");
-
+var likedDates = JSON.parse(localStorage.getItem("likes"));
 
 var randomButton = document.getElementById("randomButton");
 var dateButton = document.getElementById("dateButton");
@@ -39,17 +37,19 @@ document.body.removeChild(searchBar);
 
 var toTop = document.getElementById("toTop");
 
+//Event listeners
 
+//Changes the page to appropriately work for the random images
 randomButton.addEventListener("click", function(){
     if(!randomButton.classList.contains("selected")){
         randomButton.classList.add("selected");
         dateButton.classList.remove("selected");
         likedButton.classList.remove("selected");
-        //TODO: Change the objects that are shown.
+
         clearArea(display);
+        url = `https://api.nasa.gov/planetary/apod?api_key=${API_TOKEN}&count=5`;
+        getData(url);
         window.addEventListener("scroll", infiniteScroll);
-        link = `https://api.nasa.gov/planetary/apod?api_key=${API_TOKEN}&count=10`;
-        getData(link);
 
         display.insertAdjacentElement("beforebegin", modeRandom);
 
@@ -60,6 +60,7 @@ randomButton.addEventListener("click", function(){
     
 });
 
+//Changes the page to appropriately work for the search feature
 dateButton.addEventListener("click", function(){
     if(!dateButton.classList.contains("selected")){
         randomButton.classList.remove("selected");
@@ -68,67 +69,48 @@ dateButton.addEventListener("click", function(){
 
         clearArea(display);
         window.removeEventListener("scroll", infiniteScroll);
+
         display.insertAdjacentElement("beforebegin", modeDates);
         display.insertAdjacentElement("beforebegin", searchBar);
-    
+        
         removeFromBody(modeRandom);
         removeFromBody(modeLiked);
+
+        searchFunction();
     }
     
 });
 
+//Changes the page to appropriately work for the liked images
 likedButton.addEventListener("click", function(){
     if(!likedButton.classList.contains("selected")){
         randomButton.classList.remove("selected");
         dateButton.classList.remove("selected");
         likedButton.classList.add("selected");
-        //TODO: Change the objects that are shown.
+
         clearArea(display);
         window.removeEventListener("scroll", infiniteScroll);
         display.insertAdjacentElement("beforebegin", modeLiked);
-
+        
         removeFromBody(searchBar);
         removeFromBody(modeRandom);
         removeFromBody(modeDates);
+        
+        if(likedDates.length >= 1){
+            modeLiked.innerHTML = "View all the images you liked!";
+            for(let i = 0; i < likedDates.length; i++){
+                url = `https://api.nasa.gov/planetary/apod?api_key=${API_TOKEN}&start_date=${likedDates[i]}&end_date=${likedDates[i]}`;
+                getData(url);
+            }
+        }else{
+            modeLiked.innerHTML = "Go like some images and come back when you do!";
+        }
+
     }
     
 });
 
-search.addEventListener("click", function(){
-    var maxDays = 31;
-    var defaultDays = 10;
-    var maxDiff = maxDays * 24 * 60 * 60 * 1000;
-    var difference = defaultDays * 24 * 60 * 60 * 1000;
-    //TODO CHECK IF NO DATE IS PICKED AND RECOMMMEND THEY USE RANDOM IMAGES
-    //Checks which dates have data
-    if(start.value != "" && end.value != ""){
-        if(Date.parse(start.value) > Date.parse(end.value)){
-            window.alert("The end date needs to be after the start date");
-            return;
-        }else if(Date.parse(end.value) - Date.parse(start.value) > maxDiff){
-            let end_value = Date.parse(start.value) + difference;
-            end.value = dateToParameter(end_value);
-            link = `https://api.nasa.gov/planetary/apod?api_key=${API_TOKEN}&start_date=${start.value}&end_date=${end.value}`;
-            window.alert("The end date has been automatically changed to " + end.value)
-        }else{
-            link = `https://api.nasa.gov/planetary/apod?api_key=${API_TOKEN}&start_date=${start.value}&end_date=${end.value}`;
-        }
-    }else if(start.value != ""){
-        let end_value = Date.parse(start.value) + difference;
-        end.value = dateToParameter(end_value);
-        link = `https://api.nasa.gov/planetary/apod?api_key=${API_TOKEN}&start_date=${start.value}&end_date=${end.value}`;
-
-    //Checks if an end date is chosen
-    }else if(end.value != ""){
-        let start_value = Date.parse(end.value) - difference;
-        start.value = dateToParameter(start_value);
-        link = `https://api.nasa.gov/planetary/apod?api_key=${API_TOKEN}&start_date=${start.value}&end_date=${end.value}`;
-    }else if(Date.parse(start.value) > Date.parse(end.value)){
-        window.alert("The end date needs to be after the start date");
-        //TODO: ADD EVENTLISTENER FOR CHANGING DATE
-    }
-    getData(link);
-});
+search.addEventListener("click", searchFunction);
 
 
 clear.addEventListener("click", function(){
@@ -137,24 +119,12 @@ clear.addEventListener("click", function(){
 });
 
 toTop.addEventListener("click", function(){
-    //Add animation maybe?
     window.scrollTo(scrollX, 0);
 });
 
 window.addEventListener("scroll", infiniteScroll);
 
-function infiniteScroll(){
-    var offset = 1000;
-    if(window.innerHeight + window.scrollY + offset >= document.body.offsetHeight){
-        window.removeEventListener("scroll", infiniteScroll);
-        getData(link);
-        setTimeout(function(){
-            window.addEventListener("scroll", infiniteScroll);
-        }, 2500);
-
-    }
-}
-//Returns an appropriate date to be a valid parameter
+//Returns an appropraite date to be a valid parameter
 function dateToParameter(milliseconds){
     //Limits end date to the current date
     if(milliseconds > Date.parse(window.today)){
@@ -166,88 +136,133 @@ function dateToParameter(milliseconds){
 
     //Returns date in yyyy-mm-dd format
     let date = new Date(milliseconds);
-    var year = date.getFullYear();  
-    var month = date.getMonth() + 1;
+    let year = date.getFullYear();  
+    let month = date.getMonth() + 1;
     if(month < 10){
         month = '0' + month;
     }
-    var day = date.getDate();
+    let day = date.getDate();
     if(day < 10){
         day = '0' + day;
     }
     
-    var string_date = year + "-" + month + "-" + day;
+    let string_date = year + "-" + month + "-" + day;
     return string_date;
 }
 
-function like(){
-    if(this.innerHTML == "Like"){
-        this.innerHTML = "Unlike";
-        likedDates.push(this.value);
-        localStorage.setItem("likes", likedDates);
-        console.log(likedDates);
+function searchFunction(){
+    var maxDays = 31;
+    var defaultDays = 10;
+    var maxDiff = maxDays * 24 * 60 * 60 * 1000;
+    var difference = defaultDays * 24 * 60 * 60 * 1000;
+    clearArea(display);
 
+    //Checks which dates have data and works appropriately
+    if(start.value != "" && end.value != ""){
+        if(Date.parse(start.value) > Date.parse(end.value)){
+            //Does not search anything if the start date is after the end date
+            window.alert("The end date needs to be after the start date");
+            return;
+
+        }else if(Date.parse(end.value) - Date.parse(start.value) > maxDiff){
+            //Changes the end date to 31 day after the start date if it is too large
+            let endValue = Date.parse(start.value) + maxDiff;
+            end.value = dateToParameter(endValue);
+            url = `https://api.nasa.gov/planetary/apod?api_key=${API_TOKEN}&start_date=${start.value}&end_date=${end.value}`;
+            window.alert("The end date has been automatically changed to " + end.value)
+        
+        }else{
+            //Executes normally if the dates are appropriate
+            url = `https://api.nasa.gov/planetary/apod?api_key=${API_TOKEN}&start_date=${start.value}&end_date=${end.value}`;
+        }
+
+    }else if(start.value != ""){
+        //Automatically picks end date
+        let endValue = Date.parse(start.value) + difference;
+        end.value = dateToParameter(endValue);
+        url = `https://api.nasa.gov/planetary/apod?api_key=${API_TOKEN}&start_date=${start.value}&end_date=${end.value}`;
+
+    }else if(end.value != ""){
+        //Automatically picks start date
+        let startValue = Date.parse(end.value) - difference;
+        start.value = dateToParameter(startValue);
+        url = `https://api.nasa.gov/planetary/apod?api_key=${API_TOKEN}&start_date=${start.value}&end_date=${end.value}`;
+   
     }else{
-        this.innerHTML = "Like";
-        likedDates.remove
+        //Shows the current date's photo of the day
+        url = `https://api.nasa.gov/planetary/apod?api_key=${API_TOKEN}&start_date=${today}&end_date=${today}`;
+    }
+
+    //Gets data from the url
+    getData(url);
+}
+
+//Allows for infinite scrolling in random images mode
+function infiniteScroll(){
+    var offset = 1000;
+    if(window.innerHeight + window.scrollY + offset >= document.body.offsetHeight){
+        window.removeEventListener("scroll", infiniteScroll);
+        getData(url);
+        
+        //Debounces the event listener when the if statement is true 
+        setTimeout(function(){
+            window.addEventListener("scroll", infiniteScroll);
+        }, 1000);
+
     }
 }
 
-function clearArea(area){
-    let child;
-    while(child = area.firstChild){
-        display.removeChild(child);
-    }
-}
-
-function getData(link){
-    var display = document.getElementById("displayArea");
-    // let oldArticle = document.getElementById().
-    $.getJSON(link, function(data){
+//Gets data from the url and outputs it to the display area
+function getData(url){
+    $.getJSON(url, function(data){
         $.each(data, function(index, picture){
             let article = document.createElement("article");
             
-            let title_element = document.createElement("h2");
-            let title_text = document.createTextNode(decodeURI(picture["title"]));
-            title_element.appendChild(title_text);
+            let titleElement = document.createElement("h3");
+            let titleText = document.createTextNode(decodeURI(picture["title"]));
+            titleElement.appendChild(titleText);
 
-            let file_element;
+            let fileElement;
             if(picture["media_type"] == "image"){
-                file_element = document.createElement("img");
-                file_element.setAttribute("src", picture['url']);
-                file_element.setAttribute("height", "350px");
+                fileElement = document.createElement("img");
+                fileElement.setAttribute("src", picture['url']);
             }else{
-                //VIDEO THUMBNAIL CLICK TO VIEW BLAH LAH BLAH
-                file_element = document.createElement("iframe");
-                file_element.setAttribute("height", "350px");
-                file_element.setAttribute("src", picture["url"]);
-                file_element.setAttribute("title", picture["title"]);
+                //Executes if media_type is a video
+                fileElement = document.createElement("iframe");
+                fileElement.setAttribute("src", picture["url"]);
+                fileElement.setAttribute("title", picture["title"]);
 
             }
 
-
-            let explanation_element = document.createElement("p");
-            let explanation_text = document.createTextNode(picture["explanation"]);
-            explanation_element.appendChild(explanation_text);
+            let explanationElement = document.createElement("p");
+            let explanationText = document.createTextNode(picture["explanation"]);
+            explanationElement.appendChild(explanationText);
 
             let date = document.createTextNode("Date posted: " + picture["date"]);
-            let date_element = document.createElement("p");
-            date_element.appendChild(date);
+            let dateElement = document.createElement("p");
+            dateElement.appendChild(date);
             
-            let button_element = document.createElement("button");
-            let button_text = document.createTextNode("Like");
-            button_element.appendChild(button_text);
-            button_element.setAttribute("value", picture['date']);
-            button_element.setAttribute("class", "likes");
+            let buttonElement = document.createElement("button");
+            let buttonText; 
+
+            //Adds the appropriate text for the button
+            if(likedDates.indexOf(picture["date"]) == -1){
+                buttonText = document.createTextNode("Like");
+            }else{
+                buttonText = document.createTextNode("Unlike");
+            }
+            buttonElement.appendChild(buttonText);
+            buttonElement.setAttribute("value", picture['date']);
+            buttonElement.setAttribute("class", "like");
             
 
-            article.appendChild(title_element);
-            article.appendChild(file_element);
-            article.appendChild(date_element);
-            article.appendChild(explanation_element);
-            button_element = article.appendChild(button_element);
+            article.appendChild(titleElement);
+            article.appendChild(fileElement);
+            article.appendChild(dateElement);
+            article.appendChild(explanationElement);
+            buttonElement = article.appendChild(buttonElement);
 
-            button_element.addEventListener("click", like);
+            buttonElement.addEventListener("click", like);
 
             display.appendChild(article);
 
@@ -255,11 +270,35 @@ function getData(link){
 
         });
     }).fail(function(jqXHR, textStatus, error) {
-        let error_element = document.createElement("p");
-        error_text = document.createTextNode(error);
-        error_element.appendChild(error_text);
-        display.appendChild(error_element);
+        let errorElement = document.createElement("p");
+        errorText = document.createTextNode(error);
+        errorElement.appendChild(errorText);
+        display.appendChild(errorElement);
     });
+}
+
+function like(){
+    if(this.innerHTML == "Like"){
+        //Adds liked image to array
+        this.innerHTML = "Unlike";
+        likedDates.push(this.value);   
+
+    }else{ 
+        //Removes unliked image from array
+        this.innerHTML = "Like";
+        let index = likedDates.indexOf(this.value);
+        likedDates.splice(index, 1);
+    }
+    //Updates localStorage
+    localStorage.setItem("likes", JSON.stringify(likedDates));
+}
+
+//Clears an area of all children
+function clearArea(area){
+    let child;
+    while(child = area.firstChild){
+        display.removeChild(child);
+    }
 }
 
 //Checks if an element exists before removing it to avoid uncaught domexceptions
